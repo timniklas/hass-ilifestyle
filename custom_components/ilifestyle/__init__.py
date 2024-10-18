72deec3f4f22e673415c29195558f05be1ec6526
timniklas <accounts@tnkoch.de>
2024-10-19 00:49:01 +0200

    Update __init__.py

--- custom_components/ilifestyle/__init__.py
+++ custom_components/ilifestyle/__init__.py
@@ -1,1 +1,45 @@
-#TODO
+from __future__ import annotations
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import Platform
+from homeassistant.core import HomeAssistant
+
+from .const import DOMAIN
+
+PLATFORMS: list[Platform] = [Platform.CAMERA, Platform.BUTTON]
+
+async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
+    """Set up Integration from a config entry."""
+
+    hass.data.setdefault(DOMAIN, {})
+
+    # Setup platforms (based on the list of entity types in PLATFORMS defined above)
+    # This calls the async_setup method in each of your entity type files.
+    await hass.config_entries.async_forward_entry_setups(config_entry, PLATFORMS);
+
+    # Return true to denote a successful setup.
+    return True
+
+
+async def _async_update_listener(hass: HomeAssistant, config_entry):
+    """Handle config options update."""
+    # Reload the integration when the options change.
+    await hass.config_entries.async_reload(config_entry.entry_id)
+
+
+async def async_unload_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    # This is called when you remove your integration or shutdown HA.
+    # If you have created any custom services, they need to be removed here too.
+
+    # Unload platforms
+    unload_ok = await hass.config_entries.async_unload_platforms(
+        config_entry, PLATFORMS
+    )
+
+    # Remove the config entry from the hass data object.
+    if unload_ok:
+        hass.data[DOMAIN].pop(config_entry.entry_id)
+
+    # Return that unloading was successful.
+    return unload_ok
