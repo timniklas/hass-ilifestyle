7794a60e092c7c67785c9bc524074393c38db903
timniklas <accounts@tnkoch.de>
2024-10-31 00:39:06 +0100

    Update __init__.py

--- custom_components/ilifestyle/__init__.py
+++ custom_components/ilifestyle/__init__.py
@@ -1,17 +1,46 @@
 from __future__ import annotations
 
+from collections.abc import Callable
+from dataclasses import dataclass
+
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 
 from .const import DOMAIN
+from .coordinator import MqttCoordinator
 
 PLATFORMS: list[Platform] = [Platform.CAMERA, Platform.BUTTON]
+
+@dataclass
+class RuntimeData:
+    """Class to hold your data."""
+
+    coordinator: DataUpdateCoordinator
+    cancel_update_listener: Callable
 
 async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
     """Set up Integration from a config entry."""
 
     hass.data.setdefault(DOMAIN, {})
+
+    # Initialise the coordinator that manages data updates from your api.
+    # This is defined in coordinator.py
+    coordinator = MqttCoordinator(hass, config_entry)
+
+    # Perform an initial data load from api.
+    # async_config_entry_first_refresh() is special in that it does not log errors if it fails
+    await coordinator.async_config_entry_first_refresh()
+
+    # Initialise a listener for config flow options changes.
+    # See config_flow for defining an options setting that shows up as configure on the integration.
+    cancel_update_listener = config_entry.add_update_listener(_async_update_listener)
+
+    # Add the coordinator and update listener to hass data to make
+    hass.data[DOMAIN][config_entry.entry_id] = RuntimeData(
+        coordinator, cancel_update_listener
+    )
 
     # Setup platforms (based on the list of entity types in PLATFORMS defined above)
     # This calls the async_setup method in each of your entity type files.
@@ -36,6 +65,10 @@ async def async_unload_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> 
     unload_ok = await hass.config_entries.async_unload_platforms(
         config_entry, PLATFORMS
     )
+
+    # Remove the config entry from the hass data object.
+    if unload_ok:
+        hass.data[DOMAIN].pop(config_entry.entry_id)
 
     # Return that unloading was successful.
     return unload_ok
